6d7ee38b999823c330cde52c2215685f89f73437
code1218 <skjil1218@gmail.com>
2024-07-25 10:37:39 +0900

    프로젝트 업로드

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Global } from "@emotion/react";
+import { Route, Routes } from "react-router-dom";
+import { reset } from "./styles/global";
+import PostPage from "./pages/basic/PostPage";
+import GetPage from "./pages/basic/GetPage";
+import PutPage from "./pages/basic/PutPage";
+import DeletePage from "./pages/basic/DeletePage";
+import MainLayout from "./components/MainLayout/MainLayout";
+import Sidebar from "./components/Sidebar/Sidebar";
+import MainContainer from "./components/MainContainer/MainContainer";
+import PostPage2 from "./pages/basic/PostPage2";
+import PromisePage from "./pages/basic/PromisePage";
+import RegisterSizePage from "./pages/basic/RegisterSizePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global styles={reset}/>
+      <MainLayout>
+        <Sidebar />
+        <MainContainer>
+          <Routes>
+            <Route path="/async/basic/post" element={<PostPage />} />
+            <Route path="/async/basic/get" element={<GetPage />} />
+            <Route path="/async/basic/put" element={<PutPage />} />
+            <Route path="/async/basic/delete" element={<DeletePage />} />
+            <Route path="/async/basic/post2" element={<PostPage2 />} />
+            <Route path="/async/basic/promise" element={<PromisePage />} />
+            <Route path="/async/basic/size/register" element={<RegisterSizePage />} />
+          </Routes>
+        </MainContainer>
+      </MainLayout>
+    </>
   );
 }
 
